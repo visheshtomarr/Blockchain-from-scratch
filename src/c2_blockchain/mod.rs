debe4c0dbe89b9deeab8183b9c7a7d2c45b53b34
visheshtomarr <visheshtomar7800@gmail.com>
2024-08-06 20:05:47 +0530

    Added boilerplate code for consensus

--- src/c2_blockchain/mod.rs
+++ src/c2_blockchain/mod.rs
@@ -4,3 +4,4 @@
 
 mod p1_header_chain;
 mod p2_extrinsic_state;
+mod p3_consensus;
---
+++ src/c2_blockchain/p3_consensus.rs
@@ -0,0 +1,92 @@
+//! We now have a hash-linked header chain that accepts simple extrinsics and tracks simple state.
+//! Now we will explore consensus. We are not looking at finality or fork choice here. Rather,
+//! we are adding validity rules. There are two common types of validity rules and we will explore both.
+//! 1. Rules to throttle authoring. In this case we will use a simple PoW.
+//! 2. Arbitrary / Political rules. Here we will implement two alternate validity rules.
+
+use crate::hash ;
+
+// We will use Rust's built-in hashing where the output type is u64. I'll make an alias
+// so that the code is slightly more readable.
+type Hash = u64 ;
+
+/// In this lesson we are introducing proof-of-work onto our blocks. We need a hash threshold.
+/// You may change this as you see fit, and I encourage you to experiment. Probably best to start
+/// high so we aren't wasting time mining. I'll start with 1 in 100 blocks being valid.
+pub const THRESHOLD: u64 = u64::max_value() / 100 ;
+
+/// In this lesson, we introduce the concept of a contentious hard fork. The fork will happen at
+/// this block height.
+const FORK_HEIGHT: u64 = 2 ;
+
+/// The header is now expanded to contain a consensus digest.
+/// For Proof of Work, the consensus digest is basically just a nonce which gets the block
+/// hash below a certain threshold. Although we could call the field `nonce` we will leave
+/// the more general `digest` term. For PoA, we would have a cryptographic signature in this field.
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+pub struct Header {
+    parent: Hash,
+    height: u64,
+    extrinsic: u64,
+    state: u64,
+    consensus_digest: u64,
+}
+
+// Here are the methods for creating new header and verifying headers.
+impl Header {
+    /// Returns a new valid genesis header.
+    fn genesis() -> Self {
+        todo!("First")
+    }
+
+    /// Create and return a valid child header.
+    fn child(&self, extrinsic: u64) -> Self {
+        todo!("Second")
+    }
+
+    /// Verify that all the given headers form a valid chain from this header to the tip.
+    ///
+    /// In addition to all the rules we had before, we now need to check that the block hash
+    /// is below a specific threshold.
+    fn verify_sub_chain(&self, chain: &[Header]) -> bool {
+        todo!("Third")
+    }
+
+    // After the blockchain ran for a while, a political rift formed in the community.
+    // (See the constant FORK_HEIGHT) which is set to 2 by default.
+    // Most community members have become obsessed over the state of the blockchain.
+    // On the one side, people believe that only blocks with even states should be valid.
+    // On the other side, people believe in only blocks with odd states.
+
+    /// Verify that the given headers form a valid chain.
+    /// In this case, "valid" means that the STATE MUST BE EVEN.
+    fn verify_sub_chain_even(&self, chain: &[Header]) -> bool {
+        todo!("Fourth")
+    }
+
+    /// Verify that the given headers form a valid chain.
+    /// In this case, "valid" means that the STATE MUST BE ODD.
+    fn verify_sub_chain_odd(&self, chain: &[Header]) -> bool {
+        todo!("Fifth")
+    }
+}
+
+/// Build and return two different chains with a common prefix.
+/// They should have the same genesis header.
+///
+/// Both chains should be valid according to the original validity rules.
+/// The first chain should be valid only according to the even rules.
+/// The second chain should be valid only according to the odd rules.
+///
+/// Return your solutions as three vectors:
+/// 1. The common prefix including genesis
+/// 2. The even suffix (non-overlapping with the common prefix)
+/// 3. The odd suffix (non-overlapping with the common prefix)
+///
+/// Here is an example of two such chains:
+///            /-- 3 -- 4
+/// G -- 1 -- 2
+///            \-- 3'-- 4'
+fn build_contentious_forked_chain() -> (Vec<Header>, Vec<Header>, Vec<Header>) {
+    todo!("Sixth")
+}
