1137e3ffb06ccc679c1fa263f92004518a113e80
visheshtomarr <visheshtomar7800@gmail.com>
2024-08-11 17:25:59 +0530

    Added boilerplate code for fork_choice

--- src/c2_blockchain/mod.rs
+++ src/c2_blockchain/mod.rs
@@ -6,3 +6,4 @@ mod p1_header_chain;
 mod p2_extrinsic_state;
 mod p3_consensus;
 mod p4_batched_extrinsics;
+mod p5_fork_choice;
---
+++ src/c2_blockchain/p5_fork_choice.rs
@@ -0,0 +1,117 @@
+//! Forks in blockchain represent alternative histories of the system.
+//! When forks arise in the blockchain, users need a way to decide which chain 
+//! they will consider best, for now. This is know as the "fork choice rule".
+//! There are several meaningful notions of "best", so we introduce a trait 
+//! that allow multiple implementations.
+//! 
+//! Since we have nothing to add to the Block or Header data structures in this lesson,
+//! we will import them from the previous lesson.
+
+use super::p4_batched_extrinsics::{Block, Header} ;
+use crate::hash ;
+
+
+const THRESHOLD: u64 = u64::max_value() / 100 ;
+
+/// Judge which blockchain is "best" when there are multiple candidates. There are several
+/// meaningful notions of "best" which is why this is a trait instead of just a
+/// method.
+pub trait ForkChoice {
+    /// Compare two chains, and return the "best" one.
+    ///
+    /// The chains are not assumed to start from the same genesis block, or even a
+    /// genesis block at all. This makes it possible to compare entirely disjoint
+    /// histories. It also makes it possible to compare _only_ the divergent part
+    /// of sibling chains back to the last common ancestor.
+    ///
+    /// The chains are assumed to be valid, so it is up to the caller to check
+    /// validity first if they are unsure.
+    fn first_chain_is_better(chain_1: &[Header], chain_2: &[Header]) -> bool;
+
+    /// Compare many chains and return the best one.
+    ///
+    /// It is always possible to compare several chains if you are able to compare
+    /// two chains. Therefore this method has a provided implementation. However,
+    /// it may be much more performant to write a fork-choice-specific implementation.
+    fn best_chain<'a>(candidate_chains: &[&'a [Header]]) -> &'a [Header] ;
+}
+
+/// The "best" chain is simply the longest chain.
+pub struct LongestChainRule ;
+
+impl ForkChoice for LongestChainRule {
+    fn first_chain_is_better(chain_1: &[Header], chain_2: &[Header]) -> bool {
+        todo!("First")
+    }
+
+    fn best_chain<'a>(candidate_chains: &[&'a [Header]]) -> &'a [Header] {
+        todo!("Second")
+    }
+}
+
+/// The "best" chain is the one with the most accumulated work.
+/// 
+/// In Proof of Work chains, each block contains a certain amount of "work".
+/// Roughly speaking, the lower the block's hash is, the more work it contains,
+/// because finding a block with a low hash requires, on average, trying more 
+/// nonces. Modeling the amount of work required to achieve a particular hash
+/// is out of scope for this exercise, so we will use the not-really-right-but
+/// conceptually-good-enough formula `work = THRESHOLD - block_hash`.
+pub struct HeaviestChainRule ;
+
+/// Mutates a block (and its embedded header) to contain more PoW difficulty.
+/// This will be useful for exploring the heaviest chain rule. The expected
+/// usage is that you create a block using the normal `Block.child()` method
+/// and then pass the block to this helper for additional mining.
+fn mine_extra_hard(block: &mut Block, threshold: u64) {
+    todo!("Third")
+}
+
+impl ForkChoice for HeaviestChainRule {
+    fn first_chain_is_better(chain_1: &[Header], chain_2: &[Header]) -> bool {
+        todo!("Fourth")
+    }
+
+    fn best_chain<'a>(candidate_chains: &[&'a [Header]]) -> &'a [Header] {
+        todo!("Fifth")
+    }
+}
+
+/// The best chain is the one with the most blocks that have even hashes.
+///
+/// This exact rule is a bit contrived, but it does model a family of fork choice rules
+/// that are useful in the real world. We just can't code them here because we haven't
+/// implemented Proof of Authority yet. Consider the following real world examples
+/// that have very similar implementations.
+///
+/// 1. Secondary authors. In each round there is one author who is supposed to author.
+///    If that author fails to create a block, there is a secondary author who may do so.
+///    The best chain is the one with the most primary-authored blocks.
+///
+/// 2. Interleaved Pow/PoA. In each round there is one author who is allowed to author.
+///    Anyone else is allowed to mine a PoW-style block. The best chain is the one with
+///    the most PoA blocks, and ties are broken by the most accumulated work.
+pub struct MostBlocksWithEvenHash ;
+
+impl ForkChoice for MostBlocksWithEvenHash {
+    fn first_chain_is_better(chain_1: &[Header], chain_2: &[Header]) -> bool {
+        todo!("Sixth")
+    }
+
+    fn best_chain<'a>(candidate_chains: &[&'a [Header]]) -> &'a [Header] {
+        todo!("Seventh")
+    }
+}
+
+/// Build and return two different chains with a common prefix.
+/// They should have the same genesis header. Both chains should be valid.
+/// The first chain should be longer (have more blocks), but the second
+/// chain should have more accumulated work.
+///
+/// Return your solutions as three vectors:
+/// 1. The common prefix including genesis
+/// 2. The suffix chain which is longer (non-overlapping with the common prefix)
+/// 3. The suffix chain with more work (non-overlapping with the common prefix)
+fn create_fork_one_side_longer_other_side_heavier() -> (Vec<Header>, Vec<Header>, Vec<Header>) {
+    todo!("Eighth")
+}
